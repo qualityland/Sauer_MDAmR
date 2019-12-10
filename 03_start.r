3fda0be887e09b53d3bcdac3805770824fb0dab5
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-12-10 16:24:14 +0100

    three chapters read, packages installed, data loaded.

---
+++ 03_start.r
@@ -0,0 +1,13 @@
+
+# download data sets from authors github repository
+#devtools::install_github("sebastiansauer/pradadata")
+
+
+library(pradadata)
+?pradadata
+
+url <- "https://raw.githubusercontent.com/sebastiansauer/modar/master/datasets/stats_test.csv"
+download.file(url, "./data/stats_test.csv")
+
+stats_test <- read.csv("./data/stats_test.csv")
+str(stats_test)
